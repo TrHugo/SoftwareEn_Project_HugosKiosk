f768c59fb0b140c1fdcc549a559988ba988d688d
Loro67 <leomanwinter@gmail.com>
2026-01-06 21:31:45 +0100

    article model modification

--- backend/src/models/article.model.js
+++ backend/src/models/article.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const articleSchema = new mongoose.Schema(
   {
     publisher_id: { type: String, required: true },
-    title: { type: Boolean, required: true },
+    title: { type: String, required: true },
     content: { type: String, required: true }
   },
   { timestamps: true }
