4eab71177977178812754357e1faf55dab750f53
Loro67 <leomanwinter@gmail.com>
2026-01-06 17:02:14 +0100

    delete doctor file

--- backend/src/routes/auto/doctor.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { listDoctors, createDoctor } from "../controllers/doctor.controller.js";
-
-const router = express.Router();
-
-router.get("/", listDoctors);
-router.post("/", createDoctor);
-
-export default router;
